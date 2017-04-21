6cd2759bb6f3d6ea6bc25a2ba0085197210d0d47
Andrew Henrique <andrewhenriquef@gmail.com>
2017-04-21 16:12:44 -0300

    Adapter pattern

---
+++ src/application/Enumeration.java
@@ -0,0 +1,9 @@
+package application;
+
+public interface Enumeration {
+
+	public boolean hasMoreElements();
+	
+	public Object nextElement();
+
+}
---
+++ src/application/Iterator.java
@@ -0,0 +1,11 @@
+package application;
+
+public interface Iterator {
+
+	public boolean hasNext();
+
+	public Object next();
+	
+	public void remove();
+
+}
